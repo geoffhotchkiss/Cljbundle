a4ecec995c1336c4f876364c4dfbd5744ff3c319
Geoff Hotchkiss <geoff@geoffhotchkiss.com>
2012-07-26 14:55:42 -0500

    Ugh

--- project.clj
+++ project.clj
@@ -2,8 +2,5 @@
   :description "Cljbundle: The Indie Bundle Manager"
   :dependencies [[org.clojure/clojure "1.4.0"]
   				 [clj-tagsoup "0.3.0"]
-  				 [enlive "1.0.1"]
-  				 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
   				 ]
-  :main cljbundle.core
-  :source-path "src")
+  :main cljbundle.core)
--- src/cljbundle/core.clj
+++ src/cljbundle/core.clj
@@ -1,13 +1,13 @@
-(ns cljbundle.core
-  (use clojure.string)
-  (use pl.danieljanus.tagsoup)
-  (use clojure.pprint)
-  (:require [net.cgrand.enlive-html :as html]))
+(ns cljbundle.core)
+
+(use 'clojure.string)
+(use 'pl.danieljanus.tagsoup)
 
 (def bundle-list [])
 
 (def parsed-page [])
 
+<<<<<<< HEAD
 (def *hb1-url* "")
 
 (defn fetch-url [url]
@@ -16,6 +16,8 @@
 (defn hb-games []
   (apply list (html/select (fetch-url *hb1-url*) [:div.download :div.flexbtn :a.a])))
 
+=======
+>>>>>>> parent of 7651d0b... Added support for enlive :-D
 (defn add-to-bundle-list [item]
   (def bundle-list (conj bundle-list item)))
 
